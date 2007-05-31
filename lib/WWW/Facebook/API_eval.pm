8573d67ac0a83389914ff0ea7c199ceef9b5dc8a
dromano <dromano@a3c37c4d-c931-0410-83b8-a36781ac0157>
2007-05-31 18:12:41 +0000

    updated API_eval branch
    git-svn-id: https://perl-www-facebook-api.googlecode.com/svn/import/trunk@24 a3c37c4d-c931-0410-83b8-a36781ac0157

--- lib/WWW/Facebook/API_eval.pm
+++ lib/WWW/Facebook/API_eval.pm
@@ -15,90 +15,74 @@ use version; our $VERSION = qv('0.1.4');
 use Moose;
 extends 'WWW::Facebook::API::Base';
 
-has 'auth' => (is => 'ro',
-    isa => 'WWW::Facebook::API::Auth',
-    default => sub {
-        use WWW::Facebook::API::Auth;
-        return WWW::Facebook::API::Auth->new( base => $_[0] )
-    },
+my %namespace = (
+    Auth            => [],
+    Login           => [],
+    Events          => [ qw/get getMembers/ ],
+    FBML            => [ qw/refreshImgSrc refreshRefUrl setRefHandle/ ],
+    Feed            => [ qw/publishStoryToUser publishActionOfUser/ ],
+    FQL             => [ qw/query/ ],
+    Friends         => [ qw/get getAppUsers areFriends/ ],
+    Groups          => [ qw/get getMembers/ ],
+    Notifications   => [ qw/get send sendRequest/ ],
+    Photos          => [ qw/addTag createAlbum get getAlbums getTags upload/ ],
+    Profile         => [ qw/setFBML getFBML/ ],
+    Update          => [ qw/decodeIDs/ ],
+    Users           => [ qw/getInfo getLoggedInUser/ ],
 );
-has 'login' => (is => 'ro',
-    isa => 'WWW::Facebook::API::Login',
-    default => sub {
-        use WWW::Facebook::API::Login;
-        return WWW::Facebook::API::Login->new( base => $_[0] )
-    },
-);
-has 'events' => (is => 'ro',
-    isa => 'WWW::Facebook::API::Events',
-    default => sub {
-        use WWW::Facebook::API::Events;
-        return WWW::Facebook::API::Events->new( base => $_[0] )
-    },
-);
-has 'fql' => (is => 'ro',
-    isa => 'WWW::Facebook::API::FQL',
-    default => sub {
-        use WWW::Facebook::API::FQL;
-        return WWW::Facebook::API::FQL->new( base => $_[0] )
-    },
-);
-has 'feed' => (is => 'ro',
-    isa => 'WWW::Facebook::API::Feed',
-    default => sub {
-        use WWW::Facebook::API::Feed;
-        return WWW::Facebook::API::Feed->new( base => $_[0] )
-    },
-);
-has 'friends' => (is => 'ro',
-    isa => 'WWW::Facebook::API::Friends',
-    default => sub {
-        use WWW::Facebook::API::Friends;
-        return WWW::Facebook::API::Friends->new( base => $_[0] )
-    },
-);
-has 'groups' => (is => 'ro',
-    isa => 'WWW::Facebook::API::Groups',
-    default => sub {
-        use WWW::Facebook::API::Groups;
-        return WWW::Facebook::API::Groups->new( base => $_[0] )
-    },
-);
-has 'notifications' => (is => 'ro',
-    isa => 'WWW::Facebook::API::Notifications',
-    default => sub {
-        use WWW::Facebook::API::Notifications;
-        return WWW::Facebook::API::Notifications->new( base => $_[0] )
-    },
-);
-has 'profile' => (is => 'ro',
-    isa => 'WWW::Facebook::API::Profile',
-    default => sub {
-        use WWW::Facebook::API::Profile;
-        return WWW::Facebook::API::Profile->new( base => $_[0] )
-    },
-);
-has 'photos' => (is => 'ro',
-    isa => 'WWW::Facebook::API::Photos',
-    default => sub {
-        use WWW::Facebook::API::Photos;
-        return WWW::Facebook::API::Photos->new( base => $_[0] )
-    },
-);
-has 'update' => (is => 'ro',
-    isa => 'WWW::Facebook::API::Update',
-    default => sub {
-        use WWW::Facebook::API::Update;
-        return WWW::Facebook::API::Update->new( base => $_[0] )
-    },
-);
-has 'users' => (is => 'ro',
-    isa => 'WWW::Facebook::API::Users',
-    default => sub {
-        use WWW::Facebook::API::Users;
-        return WWW::Facebook::API::Users->new( base => $_[0] )
-    },
-);
+
+my $create_package_code = sub {
+    local $_ = shift;
+    my $attr = shift;
+    eval qq(
+        package WWW::Facebook::API::$_;
+        use Moose;
+        use Carp;
+        extends 'Moose::Object';
+
+        has 'base' => (is => 'ro', isa => 'WWW::Facebook::API::Base');
+
+        for ( qw/@_/ ) {
+            (my \$sub_name = \$_ ) =~ s!([a-z])([A-Z])!\${1}_\\l\$2!g;
+            eval qq/
+                sub \$sub_name { shift->base->call( "$attr.\$_", \\\@_ ); }
+                /;
+            croak "Cannot create sub \$sub_name in $_: \$@\n" if \$@;
+        }
+    );
+    croak "Cannot create namespace $_ $@\n" if $@;
+};
+
+my $create_attribute_code = sub {
+    local $_        = shift;
+    my $attribute   = shift;
+    my $eval_hook   = shift;
+    eval qq(
+        package WWW::Facebook::API;
+
+        has '$attribute' => (is => 'ro',
+            isa => 'WWW::Facebook::API::$_',
+            default => sub {
+                $eval_hook;
+                return WWW::Facebook::API::$_->new( base => \$_[0] )
+            },
+        );
+    );
+    croak "Cannot create attribute $attribute: $@\n" if $@;
+};
+
+for (keys %namespace) {
+    my $attribute = "\L$_";
+    my $attr_code_hook = '';
+    if ( @{$namespace{$_}} ) {
+        $create_package_code->( $_, $attribute, @{$namespace{$_}} );
+    }
+    else {
+        $attr_code_hook = "use WWW::Facebook::API::$_";
+    }
+
+    $create_attribute_code->( $_, $attribute, $attr_code_hook );
+}
 
 has 'simple' => (is => 'rw',
     isa => 'Bool',
