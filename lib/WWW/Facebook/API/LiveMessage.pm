356d53e0a328e7e057ef4adf47eb8efba1a0a7f4
David Romano <unobe@cpan.org>
2009-08-21 20:54:47 -0700

    * Added LiveMessage namespace and test
    new file:   lib/WWW/Facebook/API/LiveMessage.pm
    new file:   t/livemessage.t

---
+++ lib/WWW/Facebook/API/LiveMessage.pm
@@ -0,0 +1,106 @@
+#######################################################################
+# ex: set ts=8 sw=4 et
+#########################################################################
+package WWW::Facebook::API::LiveMessage;
+
+use warnings;
+use strict;
+use Carp;
+
+sub send  { return shift->base->call( 'liveMessage.send',  @_ ) }
+
+1;    # Magic true value required at end of module
+__END__
+
+=head1 NAME
+
+WWW::Facebook::API::LiveMessage - Facebook LiveMessage
+
+=head1 SYNOPSIS
+
+    use WWW::Facebook::API::LiveMessage;
+
+=head1 DESCRIPTION
+
+Methods for accessing live messaging with L<WWW::Facebook::API>
+
+=head1 SUBROUTINES/METHODS 
+
+=over
+
+=item base
+
+Returns the L<WWW::Facebook::API> base object.
+
+=item new
+
+Constructor.
+
+=item send( %params )
+
+The liveMessage.send method of the Facebook API:
+
+    $response = $client->livemessage->send(
+        recipient => $uid,
+        event_name => $event_name,
+        message => $message,
+    );
+
+=head1 DIAGNOSTICS
+
+None.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+WWW::Facebook::API::LiveMessage requires no configuration files or environment
+variables.
+
+=head1 DEPENDENCIES
+
+See L<WWW::Facebook::API>
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-www-facebook-api@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+David Romano  C<< <unobe@cpan.org> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2009, David Romano C<< <unobe@cpan.org> >>. All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENSE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
